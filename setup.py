34747868f1f427dc6dca46f446419c172420bdbf
Collin <Sage>
2020-06-23 14:52:08 -0400

    Bump version number for release.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/MrSage/django_api_contracts'
 EMAIL = 'clln.sage@yahoo.com'
 AUTHOR = 'Collin Sage'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.4.2'
+VERSION = '0.4.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
